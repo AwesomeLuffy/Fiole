a1815073a37503060c06736f388f59d87e89d58e
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-13 17:49:41 -0500

    Adding  - FieldsType enum - REGEX - verify_field method

--- src/Model/Utils.py
+++ src/Model/Utils.py
@@ -1,7 +1,22 @@
-from config import WEBSITE_NAME
+from typing import Match
+
+from app_config import WEBSITE_NAME
+from enum import Enum
+import re
+
+
+class FieldsType(Enum):
+    DA = 1
+    NAME = 2
+    FILE = 3
 
 
 class Utils:
+    PATTERN_REGEX_DA = re.compile("^[0-9]{9,12}$")
+
+    PATTERN_REGEX_NAME = re.compile("^[a-zA-ZÀ-ÿ\\-\\s]{1,40}$")
+
+    LIST_ACCEPT_FILES = [".jpg", ".jpeg", ".png"]
 
     # This method is used to preformat a dict with all needed parameters for the render_template method
     # That allow to not have to say the website name each time per example
@@ -13,3 +28,15 @@ class Utils:
     @staticmethod
     def get_custom_preformat_render(template, **kwargs):
         return {"template_name_or_list": template, "title": WEBSITE_NAME, **kwargs}
+
+    # Method to verify field from a form
+    @staticmethod
+    def verify_field(field: str, field_type: FieldsType) -> Match[str] | None | bool:
+        if field_type == FieldsType.DA:
+            return Utils.PATTERN_REGEX_DA.match(field)
+        elif field_type == FieldsType.NAME:
+            return Utils.PATTERN_REGEX_NAME.match(field)
+        elif field_type == FieldsType.FILE:
+            return field.split(".")[-1] not in Utils.LIST_ACCEPT_FILES
+        else:
+            return False
