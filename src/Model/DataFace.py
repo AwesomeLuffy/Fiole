87e4738b3e7ae7258526b261f44d8bdf89d183d3
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-13 17:49:08 -0500

    Adding DataFace object

--- src/Model/DataFace.py
+++ src/Model/DataFace.py
@@ -1,4 +1,18 @@
+import numpy
+
+
 class DataFace:
 
-    def __init__(self):
+    def __init__(self, da: int, img, name: str, surname: str, have_access: bool):
+        self.name = name
+        self.surname = surname
+        self.have_access = have_access
+        self.img = img
+        self.da = da
+        self.face_encoded = None
+
+    def build_face_encoded(self, face_encoded):
+        nd = numpy.ndarray
+        nd.dumps()
         pass
+
