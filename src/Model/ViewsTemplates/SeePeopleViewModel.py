ebd8a74e33e2a870cf208be38376634797a18f3a
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-16 16:47:39 -0500

    Adding body of SeePeopleViewModel.py

--- src/Model/ViewsTemplates/SeePeopleViewModel.py
+++ src/Model/ViewsTemplates/SeePeopleViewModel.py
@@ -1,4 +1,16 @@
+from flask import Markup
+
+
 class SeePeopleViewModel:
 
-    def __init__(self, dataface_list: list):
+    def __init__(self, dataface_list: list, list_error: list = None, postback: bool = False):
         self.data_list = dataface_list
+
+        self.postback = postback
+        if self.postback:
+            self.is_error: bool = list_error is not []
+            if self.is_error:
+                errors_message = '<br>'.join(list_error)
+                self.message = Markup(f"Error for : {errors_message}")
+            else:
+                self.message = f"Success !"
