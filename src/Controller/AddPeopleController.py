d043324b2a99b59de616dcd9811b0f32f8c617be
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-16 18:06:23 -0500

    Fix issue -> Not able to insert data when "have_access" is checked

--- src/Controller/AddPeopleController.py
+++ src/Controller/AddPeopleController.py
@@ -3,6 +3,8 @@ from src.Model.Utils import Utils, FieldsType
 from flask import request
 from src.Model.DataFace import DataFace
 
+from werkzeug.datastructures import ImmutableMultiDict
+
 from src.Model.ViewsTemplates.AddPeopleViewModel import AddPeopleViewModel
 from src.Model.FacesDB import FacesDB
 
@@ -19,11 +21,15 @@ class AddPeopleController:
             # For example: inputData0, inputName0, inputFile0 will be split into 1 array
             # And inputData1, inputName1, inputFile1 will be split into another array
 
-            data = request.form.items()
+            # To be sure to execute the "if" that import the data, we add a field with a space as key, it will not be
+            # handled by the for loop but this allows to be sure to execute the last line field from the form
+            # So, I convert the ImmutableMultiDict to a normal dict, add the space field and convert it back
+            # to ImmutableMultiDict
+            data = ImmutableMultiDict(request.form.copy().add(" ", " "))
+
             return_error_string = []
             actual_form = 0
             data_faces = []
-            field_counter = 0
             success_added_count = 0
 
             separated_form = {}
@@ -33,27 +39,8 @@ class AddPeopleController:
                 # If the number at the end changed or if a field not valid we check the another
                 # line of fields
                 if not key.endswith(str(actual_form)):
-                    actual_form += 1
-                    separated_form = {}
-
-                # Due to separated_from passed by reference we need to force the call of "check_for_error" so we can't
-                # use the "or" operator
-                if Utils.check_for_error(key, value, separated_form, return_error_string, actual_form):
-                    actual_form += 1
-                    separated_form = {}
-
-                field_counter += 1
-                # We only have to check DA/NAME/SURNAME field, the other one can be checked in a row
-                if field_counter == 3:
-                    field_counter = 0
-
-                    # In case of the field flexSwitch not here we set it to false
-                    if key.startswith("flexSwitchCheckAccess"):
-                        # The last value of a line is the checkbox, so we check if it's checked or not
-                        separated_form["have_access"] = True if value == "on" else False
-                    else:
-                        separated_form["have_access"] = False
-                    if Utils.verify_field(request.files[f"formFile{actual_form}"].filename, FieldsType.FILE):
+                    if Utils.verify_field(request.files[f"formFile{actual_form}"].filename,
+                                          FieldsType.FILE) and return_error_string == []:
                         file = request.files[f"formFile{actual_form}"]
                         file.filename = f"{separated_form['da']}.{file.filename.split('.')[-1]}"
 
@@ -61,17 +48,30 @@ class AddPeopleController:
                         file.save(path)
                         separated_form["img_path"] = path
 
+                        if "have_access" not in separated_form:
+                            separated_form["have_access"] = False
+
                         data_face = DataFace(**separated_form)
-                        is_error, error = data_face.build_face_encoded()
+                        is_success, error = data_face.build_face_encoded()
 
-                        if is_error:
-                            data_faces.append(DataFace(**separated_form))
+                        if is_success:
+                            data_faces.append(data_face)
                             success_added_count += 1
-                            continue
+                        else:
+                            print(error)
+                            return_error_string.append(f"Line ({actual_form}) for DA {separated_form['da']} : {error}")
+                    actual_form += 1
+                    separated_form = {}
 
-                        return_error_string.append(f"Line ({actual_form}) for DA {separated_form['da']} : {error}")
+                # Due to separated_from passed by reference we need to force the call of "check_for_error" so we can't
+                # use the "or" operator
+                Utils.check_for_error(key, value, separated_form, return_error_string, actual_form)
 
-            FacesDB.insert_faces_db(data_faces)
+                if key.startswith("flexSwitchCheckAccess"):
+                    separated_form["have_access"] = True
+
+            if len(data_faces) > 0:
+                FacesDB.insert_faces_db(data_faces)
 
             preformat = Utils.get_custom_preformat_render('add_people.html',
                                                           obj=AddPeopleViewModel(success_added_count,
@@ -80,4 +80,3 @@ class AddPeopleController:
         else:
             preformat = Utils.get_custom_preformat_render('add_people.html', obj=None, script='add_input_fields.js')
         return render_template(**preformat)
-
