c5ea298c01ca6f64ea82648ca65512f5a3356f5e
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-16 18:07:17 -0500

    Adding constant to use the parameters of the constructor and unpack it from a dict

--- src/Model/DataFace.py
+++ src/Model/DataFace.py
@@ -2,6 +2,11 @@ import face_recognition
 
 
 class DataFace:
+    DA_PARAMETER_DF = "da"
+    NAME_PARAMETER_DF = "name"
+    SURNAME_PARAMETER_DF = "surname"
+    HAVE_ACCESS_PARAMETER_DF = "have_access"
+    IMG_PATH_PARAMETER_DF = "img_path"
 
     def __init__(self, da: int, img_path, name: str, surname: str, have_access: bool):
         self.name = name
@@ -13,6 +18,11 @@ class DataFace:
         self.face_encoded_bytes = None
 
     def build_face_encoded(self) -> tuple[bool, str]:
+        """
+        Build the face encoded from the image path
+        Return true if success, false if not
+        :return: tuple[bool, str]
+        """
         try:
             image = face_recognition.load_image_file(self.img_path)
             self.face_encoded = face_recognition.face_encodings(image)[0]
