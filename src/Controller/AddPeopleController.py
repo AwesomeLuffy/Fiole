056ad298636fd7c70efc0b67e300b7775069c08f
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-16 11:39:45 -0500

    Adding constructor Set method as static Handle error in case no face are in an image

--- src/Controller/AddPeopleController.py
+++ src/Controller/AddPeopleController.py
@@ -4,11 +4,16 @@ from flask import request
 from src.Model.DataFace import DataFace
 
 from src.Model.ViewsTemplates.AddPeopleViewModel import AddPeopleViewModel
+from src.Model.FacesDB import FacesDB
 
 
 class AddPeopleController:
 
-    def add(self=None):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add():
         if request.method == 'POST':
             # Get the data from the form and split it into array by the number at the end of the field
             # For example: inputData0, inputName0, inputFile0 will be split into 1 array
@@ -27,11 +32,18 @@ class AddPeopleController:
 
                 # If the number at the end changed or if a field not valid we check the another
                 # line of fields
-                if not key.endswith(str(actual_form)) or AddPeopleController.check_for_error(
-                        key, value, separated_form, return_error_string, actual_form):
+                if not key.endswith(str(actual_form)):
                     actual_form += 1
+                    separated_form = {}
+
+                # Due to separated_from passed by reference we need to force the call of "check_for_error" so we can't
+                # use the "or" operator
+                if AddPeopleController.check_for_error(key, value, separated_form, return_error_string, actual_form):
+                    actual_form += 1
+                    separated_form = {}
 
                 field_counter += 1
+                # We only have to check DA/NAME/SURNAME field, the other one can be checked in a row
                 if field_counter == 3:
                     field_counter = 0
 
@@ -45,17 +57,25 @@ class AddPeopleController:
                         file = request.files[f"formFile{actual_form}"]
                         file.filename = f"{separated_form['da']}.{file.filename.split('.')[-1]}"
 
-                        file.save(f"src/static/img/{file.filename}")
-                        separated_form["img"] = request.files[f"formFile{actual_form}"].filename
+                        path = f"src/static/img/{file.filename}"
+                        file.save(path)
+                        separated_form["img_path"] = path
 
-                        print(type(file))
+                        data_face = DataFace(**separated_form)
+                        is_error, error = data_face.build_face_encoded()
 
-                        data_faces.append(DataFace(**separated_form))
+                        if is_error:
+                            data_faces.append(DataFace(**separated_form))
+                            success_added_count += 1
+                            continue
 
-                        success_added_count += 1
+                        return_error_string.append(f"Line ({actual_form}) for DA {separated_form['da']} : {error}")
+
+            FacesDB.insert_faces_db(data_faces)
 
             preformat = Utils.get_custom_preformat_render('add_people.html',
-                                                          obj=AddPeopleViewModel(success_added_count, False),
+                                                          obj=AddPeopleViewModel(success_added_count,
+                                                                                 return_error_string),
                                                           script='add_input_fields.js')
         else:
             preformat = Utils.get_custom_preformat_render('add_people.html', obj=None, script='add_input_fields.js')
@@ -66,17 +86,17 @@ class AddPeopleController:
                         actual_line: int) -> bool:
         if key.startswith("inputDA"):
             if not Utils.verify_field(value, FieldsType.DA):
-                return_error_string.append(f"Line ({actual_line}) : DA is not valid")
+                return_error_string.append(f"Line ({actual_line}) : DA is not valid ({value})")
                 return True
             array_df_constructor["da"] = value
         if key.startswith("inputName"):
             if not Utils.verify_field(value, FieldsType.NAME):
-                return_error_string.append(f"Line ({actual_line}) : Name is not valid")
+                return_error_string.append(f"Line ({actual_line}) : Name is not valid ({value})")
                 return True
             array_df_constructor["name"] = value
         if key.startswith("inputFName"):
             if not Utils.verify_field(value, FieldsType.NAME):
-                return_error_string.append(f"Line ({actual_line}) : First Name is not valid")
+                return_error_string.append(f"Line ({actual_line}) : Fuke Name is not valid ({value})")
                 return True
             array_df_constructor["surname"] = value
 
