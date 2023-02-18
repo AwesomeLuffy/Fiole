1ed4daa4584a57d264c52b5b56c05e5a8afc5f54
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-17 20:36:08 -0500

    Adding Log in et log out route

--- src/Routes/Router.py
+++ src/Routes/Router.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from src.Controller.HomeController import HomeController
 from src.Controller.AddPeopleController import AddPeopleController
 from src.Controller.SeePeopleController import SeePeopleController
+from src.Controller.LoginController import LoginController
 
 # The parameter in route will define the url like if /mywebsite is set, the url will be like localhost:XXXX/mywebsite
 
@@ -15,5 +16,10 @@ AddPeopleRouter.route('/add', methods=['GET', 'POST'])(AddPeopleController.add)
 
 # Create a router for the unknows page
 SeePeopleRouter = Blueprint('SeeRouter', __name__)
-SeePeopleRouter.route('/unknowns', methods=['GET', 'POST'])(SeePeopleController.unknows)
+SeePeopleRouter.route('/unknowns', methods=['GET', 'POST'])(SeePeopleController.unknown)
 SeePeopleRouter.route('/registered', methods=['GET', 'POST'])(SeePeopleController.registered)
+
+# Create a router for the login page
+LoginRouter = Blueprint('LoginRouter', __name__)
+LoginRouter.route('/login', methods=['GET', 'POST'])(LoginController.login)
+LoginRouter.route('/logout', methods=['GET'])(LoginController.logout)
