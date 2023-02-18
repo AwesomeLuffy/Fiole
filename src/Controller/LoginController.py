627c3b33b69e52a2a656b82724786469fc2362bb
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-18 18:11:32 -0500

    Moving body of login controller to login model

--- src/Controller/LoginController.py
+++ src/Controller/LoginController.py
@@ -1,16 +1,12 @@
 from flask import request, redirect, url_for, render_template, session
 from src.Model.Utils.Utils import Utils
 from src.Model.ViewsTemplates.LoginViewModel import LoginViewModel
-from src.Model.Utils.DBCommunicator import DBCommunicator
-from src.Model.Utils.JWToken import JWToken
+from src.Model.LoginModel import LoginModel
 
 
 class LoginController:
-    TOKEN_HEADER = {
-        "alg": "HS256",
-        "typ": "JWT"
-    }
-    SECRET_KEY = "secret"
+    # In days
+    TOKEN_VALIDITY = 3
 
     def __init__(self):
         pass
@@ -21,57 +17,44 @@ class LoginController:
         It will check if the user exists and if the password is correct and redirect depending on the result
         :return: render_template
         """
-        if request.method == "POST":
-            if request.form['submit'] == "login":
-                if DBCommunicator.check_user_exists(request.form['inputUsername']):
-                    if DBCommunicator.check_user_encoded_password(request.form['inputUsername'],
-                                                                  request.form['inputPassword']):
-                        session['is_connected'] = True
-                        session['username'] = request.form['inputUsername']
-
-                        response = redirect(url_for('IndexRouter.home'))
-
-                        if request.form.get('flexSwitchCheckRememberMe'):
-                            PAYLOAD = {
-                                "username": request.form['inputUsername'],
-                            }
-                            flask_token = JWToken.generate_jw_token(LoginController.TOKEN_HEADER, PAYLOAD,
-                                                                    LoginController.SECRET_KEY)
-
-                            response.set_cookie("flask_token", str(flask_token))
-
-                        return response
-                    else:
-                        preformat = Utils.get_preformat_render("login.html",
-                                                               obj=LoginViewModel(
-                                                                   is_password_valid=False,
-                                                                   is_username_valid=True,
-                                                                   username_entered=request.form['inputUsername']
-                                                               ))
-                        return render_template(**preformat)
-                else:
-                    preformat = Utils.get_preformat_render("login.html",
-                                                           obj=LoginViewModel(
-                                                               is_password_valid=False,
-                                                               is_username_valid=False,
-                                                               username_entered=request.form['inputUsername']
-                                                           ))
-                    return render_template(**preformat)
-            else:
-                preformat = Utils.get_preformat_render("login.html")
-                return render_template(**preformat)
-        else:
-            if session.get("is_connected"):
+        # Check if the user is already connected
+        if session.get("is_connected"):
+            return redirect(url_for('IndexRouter.home'))
+        # Check if the user has a token and if it is valid connect the user
+        elif request.cookies.get("flask_token"):
+            is_token_valid, token_username = LoginModel.check_login_token(request.cookies.get("flask_token"))
+            if is_token_valid:
+                session['is_connected'] = True
+                session['username'] = token_username
                 return redirect(url_for('IndexRouter.home'))
 
-            if request.cookies.get("flask_token"):
-                flask_token = request.cookies.get("flask_token")
-                token = JWToken.token_from_string(flask_token)
-                if token.check_token_signature(LoginController.SECRET_KEY):
-                    if not token.is_expired():
-                        session['is_connected'] = True
-                        session['username'] = "admin"
-                        return redirect(url_for('IndexRouter.home'))
+        # ----------------------------
+
+        if request.method == "POST":
+            if request.form['submit'] == "login":
+                username = request.form['inputUsername']
+                is_username_valid, is_password_valid = LoginModel.check_login(username,
+                                                                              request.form['inputPassword'])
+
+                if is_username_valid and is_password_valid:
+                    session['is_connected'] = True
+                    session['username'] = username
+
+                    response = redirect(url_for('IndexRouter.home'))
+
+                    if request.form.get('flexSwitchCheckRememberMe'):
+                        token = LoginModel.get_login_token(username, 3600)
+                        response.set_cookie("flask_token", token)
+
+                    return response
+
+                preformat = Utils.get_preformat_render("login.html")
+                # Add object to preformat
+                preformat["obj"] = LoginViewModel(is_username_valid=is_username_valid,
+                                                  is_password_valid=is_password_valid,
+                                                  username_entered=username)
+                return render_template(**preformat)
+        else:
             preformat = Utils.get_preformat_render("login.html")
             return render_template(**preformat)
 
---
+++ src/Model/LoginModel.py
@@ -0,0 +1,46 @@
+from src.Model.Utils.DBHandlerManager import DBHandlerManager
+from src.Model.Utils.JWToken import JWToken
+
+
+class LoginModel:
+    TOKEN_HEADER = {
+        "alg": "HS256",
+        "typ": "JWT"
+    }
+    SECRET_KEY = "secret"
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def check_login(username: str, password: str) -> tuple[bool, bool]:
+        """Method to check if the login is valid
+        :param username: username
+        :param password: password
+        :return: tuple[bool, bool] (is_username_valid, is_password_valid)
+        """
+        if DBHandlerManager.check_user_exists(username):
+            if DBHandlerManager.check_user_encoded_password(username,
+                                                            password):
+                return True, True
+            else:
+                return True, False
+        else:
+            return False, False
+
+    @staticmethod
+    def check_login_token(token: str) -> tuple[bool, str]:
+        token = JWToken.token_from_string(token)
+        if token.check_token_signature(LoginModel.SECRET_KEY):
+            if not token.is_expired():
+                return True, token.read_payload()["username"]
+        return False, ""
+
+    @staticmethod
+    def get_login_token(username: str, validity: int) -> str:
+        token = JWToken.generate_jw_token(header=LoginModel.TOKEN_HEADER,
+                                          payload={"username": username},
+                                          secret=LoginModel.SECRET_KEY,
+                                          validity=validity
+                                          )
+        return str(token)
