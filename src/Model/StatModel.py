9f1bf248dc885289220b358f009a6d4b39993614
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-22 16:36:27 -0500

    Manage thread to output what it do with the raspberry (connexion timeout, refused, camera stopped, etc..)

--- src/Model/StatModel.py
+++ src/Model/StatModel.py
@@ -5,6 +5,8 @@ from src.Model.Utils.Client import Client
 from src.Model.Utils.JWToken import JWToken
 from _thread import *
 from enum import Enum
+from src.Model.Utils.DBHandlerManager import DBHandlerManager
+import socket
 
 
 class Actions(Enum):
@@ -14,7 +16,6 @@ class Actions(Enum):
 
 
 class StatModel:
-
     MAX_UNKNOWN = 999
 
     TOKEN_HEADER = {
@@ -23,37 +24,51 @@ class StatModel:
     }
     TOKEN_SECRET = "secret"
 
+    SERVER_RESPONSE = ""
+
     def __init__(self):
         pass
 
     @staticmethod
     def get_access_count() -> tuple[int, int]:
-        values = DatabaseHandler.read_values("SELECT acces FROM faces")
+        values = DatabaseHandler.read_values(f"SELECT acces FROM {DBHandlerManager.MYSQL_FACE_TABLE}")
         # Convert list of tuple to list
         values = [value[0] for value in values]
         return values.count(True), values.count(False)
 
     @staticmethod
-    def get_number_of_unknown():
-        return DatabaseHandler.read_values("SELECT COUNT(*) FROM unknows")[0][0]
+    def get_number_of_unknown() -> int:
+        return DatabaseHandler.read_values(f"SELECT COUNT(*) FROM {DBHandlerManager.MYSQL_UNKNOWN_TABLE}")[0][0]
 
     @staticmethod
-    def actualize_face_on_raspberry():
-        return_msg = ""
-        def communicate_with_raspberry():
+    def action_on_raspberry(act: Actions) -> str:
+        try:
+            communication = threading.Thread(target=StatModel.communicate_with_raspberry, args=(act,))
+            communication.start()
+            communication.join()
+            return StatModel.SERVER_RESPONSE
+        except Exception:
+            pass
+        finally:
+            StatModel.SERVER_RESPONSE = ""
+
+    @staticmethod
+    def communicate_with_raspberry(act: Actions):
+        try:
             clt = Client(start=True)
             token_with_data = JWToken.generate_jw_token(header=StatModel.TOKEN_HEADER,
-                                                        payload={"action": Actions.STOP_CAMERA.value},
+                                                        payload={"action": act.value},
                                                         secret=StatModel.TOKEN_SECRET,
                                                         validity=1)
             clt.send(str(token_with_data))
 
-            return_msg = clt.receive()
+            StatModel.SERVER_RESPONSE = clt.receive()
+            if StatModel.SERVER_RESPONSE == "":
+                StatModel.SERVER_RESPONSE = "No response from the raspberry, timeout"
             clt.close()
-        communication = threading.Thread(target=communicate_with_raspberry)
-        communication.start()
-        communication.join()
-        print("End of communication with raspberry")
-
-
-
+        except socket.timeout:
+            StatModel.SERVER_RESPONSE = "No response from the raspberry, timeout"
+        except ConnectionRefusedError:
+            StatModel.SERVER_RESPONSE = "Connection refused by the raspberry, is it running ?"
+        except Exception:
+            StatModel.SERVER_RESPONSE = "An error occurred while communicating with the raspberry"
