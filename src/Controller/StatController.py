c925010a71c0f1e3348ac0c18e88dc0c0f4e3834
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-22 00:01:36 -0500

    Button to actualize raspberry data

--- src/Controller/StatController.py
+++ src/Controller/StatController.py
@@ -1,7 +1,8 @@
 from src.Model.Utils.Utils import Utils
-from flask import render_template as render
+from flask import render_template as render, session, redirect, url_for, request
 from src.Model.ViewsTemplates.StatViewModel import StatViewModel
 from src.Model.StatModel import StatModel
+from src.Controller.LoginController import LoginController
 
 
 class StatController:
@@ -14,6 +15,11 @@ class StatController:
         """Method to render the stat page
         :return: render_template
         """
+
+        if request.method == "POST":
+            if not session.get(LoginController.SESSION_IS_CONNECTED):
+                return redirect(url_for("LoginRouter.login"))
+            StatModel.actualize_face_on_raspberry()
 
         preformat_dict = Utils.get_preformat_render('stats.html', obj=StatViewModel(
             StatModel.get_access_count(),
